67f5eabd96709172b190e0232e4a5b43a1670678
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-10-07 21:36:36 -0300

    Add UserRepository

---
+++ server/bookspace.Api/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using bookspace.Api.Data;
+using bookspace.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookspace.Api.Repositories
+{
+    public class UserRepository : BaseRepository<User>
+    {
+        public UserRepository(BookspaceContext context) : base(context)
+        {
+        }
+
+        public async Task<User> GetByEmail(string email)
+        {
+            
+        }
+    }
+}
