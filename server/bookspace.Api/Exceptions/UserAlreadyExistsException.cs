78f1b8ddabc8fad073bd45eced67078a91bfd4fd
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-10-12 19:39:09 -0300

    Add UserAlreadyExists exception

---
+++ server/bookspace.Api/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookspace.Api.Exceptions
+{
+    public class UserAlreadyExistsException : Exception
+    {
+        public UserAlreadyExistsException()
+        {
+        }
+
+        public UserAlreadyExistsException(string message) : base(message)
+        {
+        }
+    }
+}
