34f2334e3868b13b7df057b9eb7fc613568e6593
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-10-12 19:38:52 -0300

    Add crypto service

---
+++ server/bookspace.Api/Services/CryptographyService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookspace.Api.Services
+{
+    public static class CryptographyService
+    {
+        static string GetSha256(string str)
+        {
+            SHA256 sha256 = SHA256.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+
+            StringBuilder sb = new StringBuilder();
+            byte[] stream = sha256.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+
+            return sb.ToString();
+        }
+    }
+}
